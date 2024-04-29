976dcccc9b6a2f4a46236a03227dfb8102cf2115
21Vijeth <vijethfernandes21.com>
2024-04-29 17:49:32 +0530

    final package upadated

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements
-
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.2"
 REPO_NAME = "Mlops_MongoDB"
-PKG_NAME= "Mongo-Connect"
+PKG_NAME= "Mongo-VConnect"
 AUTHOR_USER_NAME = "21Vijeth"
 AUTHOR_EMAIL = "vijethfernandes21@gmail.com"
 
@@ -39,5 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("./requirements_dev.txt"),
+    # install_requires=get_requirements("./requirements_dev.txt"),
     )
--- src/mongodb_connect/mongo_crud.py
+++ src/mongodb_connect/mongo_crud.py
@@ -1,7 +1,7 @@
 from typing import Any
 import os
 import pandas as pd
-import pymongo
+from pymongo.mongo_client import MongoClient
 import json
 from ensure import ensure_annotations
 
