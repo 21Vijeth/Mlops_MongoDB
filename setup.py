b48ea87890c69e125758bbc6d7a708c7ffa3d0f1
21Vijeth <vijethfernandes21.com>
2024-05-01 13:45:24 +0530

    Final Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.6"
+__version__ = "0.0.61"
 REPO_NAME = "Mlops_MongoDB"
 PKG_NAME= "mongo_Vconnect"
 AUTHOR_USER_NAME = "21Vijeth"
@@ -25,4 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requirs=["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensure","pytest"]
     )
